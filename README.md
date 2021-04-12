3e71d954fb02a6f190f9c3bcf7de6e0089485f80
github username <github email address>
2021-04-12 17:14:27 -0400

    review code

--- README.md
+++ README.md
@@ -15,15 +15,15 @@ This will create a server storing all of our lost toy data with restful routes
 at `http://localhost:3000/toys`. You can also check out
 `http://localhost:3000/toys/:id`
 
-## Fetch Andy's Toys
+<!-- ## Fetch Andy's Toys
 
 On the `index.html` page, there is a `div` with the `id` "toy-collection."
 
 When the page loads, make a 'GET' request to fetch all the toy objects. With the
 response data, make a `<div class="card">` for each toy and add it to the
-toy-collection `div`.
+toy-collection `div`. -->
 
-## Add Toy Info to the Card
+<!-- ## Add Toy Info to the Card
 
 Each card should have the following child elements:
 
@@ -41,9 +41,9 @@ After all of that, the toy card should resemble:
     <p>4 Likes </p>
     <button class="like-btn">Like <3</button>
   </div>
-```
+``` -->
 
-## Add a New Toy
+<!-- ## Add a New Toy
 
 When a user submits the toy form, two things should happen:
 
@@ -71,7 +71,7 @@ body: JSON.stringify({
 })
 ```
 
-For examples, refer to the [documentation](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Supplying_request_options).
+For examples, refer to the [documentation](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Supplying_request_options). -->
 
 ## Increase Toy's Likes
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let addToy = false;
+const toyForm = document.querySelector(".add-toy-form")
+const toyCollection = document.querySelector("#toy-collection")
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +15,75 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function fetchAllToys(){
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(toysArr => {
+    toysArr.forEach(toyObj => {
+      renderEachCard(toyObj)
+    })
+  })
+}
+
+function renderEachCard(toyObj){
+  const outerDiv = document.createElement('div')
+  outerDiv.classList.add("card")
+  outerDiv.dataset.id = toyObj.id
+  outerDiv.innerHTML = `
+    <h2>${toyObj.name}</h2>
+    <img src=${toyObj.image} class="toy-avatar" />
+    <p>${toyObj.likes} Likes </p>
+    <button class="like-btn">Like <3</button>
+  `
+  toyCollection.append(outerDiv)
+}
+
+toyForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const newToyObj = {
+    name: event.target.name.value,
+    image: event.target.image.value,
+    likes: 0
+  }
+  fetch('http://localhost:3000/toys', {
+    method: "POST",
+    headers: 
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(newToyObj)
+  })
+  .then(resp => resp.json())
+  .then(newToyObj => renderEachCard(newToyObj))
+  toyForm.reset()
+})
+
+toyCollection.addEventListener('click', (event) => {
+  if (event.target.className === "like-btn") likeToy(event.target)
+})
+
+function likeToy(toyElement){
+  const cardDiv = toyElement.closest("div.card")
+  const likesTag = cardDiv.querySelector("p")
+  let currentLikes = parseInt(likesTag.textContent)
+  
+  fetch(`http://localhost:3000/toys/${cardDiv.dataset.id}`, {
+    method: "PATCH",
+    headers: 
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      likes: currentLikes + 1
+    })
+  })
+  .then(resp => resp.json())
+  .then(data => {
+    likesTag.textContent = `${data.likes} Likes`
+  })
+}
+
+fetchAllToys()
